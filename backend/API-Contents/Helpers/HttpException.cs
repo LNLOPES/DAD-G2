88e1a41a4c4810206196a49ad323f2cde38cb368
lgacerbi <lgacerbi@gmail.com>
2022-04-10 13:36:25 -0300

    feat: custom http exception handler

---
+++ backend/API-Contents/Helpers/HttpException.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API_Contents.Helpers
+{
+    public class HttpException : Exception
+    {
+        public HttpException(int statusCode, string message) =>
+            (StatusCode, Message) = (statusCode, message);
+
+        public int StatusCode { get; }
+        public string? Message { get; }
+    }
+
+    public class HttpResponseExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order => int.MaxValue - 10;
+
+        public void OnActionExecuting(ActionExecutingContext context) { }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Exception is HttpException httpResponseException)
+            {
+                context.Result = new ObjectResult(httpResponseException.Message)
+                {
+                    StatusCode = httpResponseException.StatusCode
+                };
+
+                context.ExceptionHandled = true;
+            }
+        }
+    }
+}
