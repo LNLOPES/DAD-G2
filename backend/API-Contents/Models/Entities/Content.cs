b650147bb83ba0fbd9c229052ee4f60b17be6805
lgacerbi <lgacerbi@gmail.com>
2022-04-10 13:39:05 -0300

    refactor: use Guid instead of int for entities Id

---
+++ backend/API-Contents/Models/Entities/Content.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Contents.Models.Entities
+{
+    public class Content
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public Guid DisciplineId { get; set; }
+        public Guid TopicId { get; set; }
+    }
+}
