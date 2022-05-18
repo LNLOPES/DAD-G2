a0a7d72132d6ab52a8ac13186678c723af796130
guilherme-cmd <54598542+guilherme-cmd@users.noreply.github.com>
2022-05-18 10:22:57 -0300

    New

---
+++ frontend/ProjetoPraticoFront/src/app/models/placeholder.model.ts
@@ -0,0 +1,25 @@
+export class Materias {
+public id:string;
+public title:string;
+public description: string;
+public teacherId: number;
+public studentsIds: Array<any>;
+      
+}
+
+export class Topicos {
+    public id:string;
+    public title:string;
+    public description: string;
+    public disciplineId: number;
+          
+}
+
+export class Contents {
+    public id:string;
+    public title:string;
+    public description: string;
+    public disciplineId: number;
+    public topicId: number;
+          
+}
